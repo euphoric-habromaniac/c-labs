0bfb1c8963565698989a9005392129b1f9387461
Pranjal <rulerofantarctica1024@gmail.com>
2025-08-11 10:42:25 +0530

    exercise 1

---
+++ Exercise 1/1.swap_values_with_temp_block.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("enter first number: ");
+    scanf("%d", &a);
+    printf("enter second number: ");
+    scanf("%d", &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("after swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
